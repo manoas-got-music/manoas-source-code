7a139ef15d64f3093b1b07b1208ebcf868305a77
jaydald <jaydald@hawaii.edu>
2025-04-29 11:14:51 -1000

    changes

--- prisma/migrations/20250425084045_update_genres_array/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `genres` column on the `Musician` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Musician" DROP COLUMN "genres",
-ADD COLUMN     "genres" TEXT[];
