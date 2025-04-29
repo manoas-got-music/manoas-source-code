ccae6cb275446015db2feb303701a5e956183258
jaydald <jaydald@hawaii.edu>
2025-04-29 11:33:31 -1000

    changes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Role, Condition } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { hash } from 'bcrypt';
 import * as config from '../config/settings.development.json';
 
@@ -7,8 +7,9 @@ const prisma = new PrismaClient();
 async function main() {
   console.log('Seeding the database');
   const password = await hash('changeme', 10);
+  
   config.defaultAccounts.forEach(async (account) => {
-    const role = account.role as Role || Role.USER;
+    const role = account.role || prisma.Role.USER;
     console.log(`  Creating user: ${account.email} with role: ${role}`);
     await prisma.user.upsert({
       where: { email: account.email },
@@ -19,12 +20,11 @@ async function main() {
         role,
       },
     });
-    // console.log(`  Created user: ${user.email} with role: ${user.role}`);
   });
+
   for (const data of config.defaultData) {
-    const condition = data.condition as Condition || Condition.good;
+    const condition = data.condition || prisma.Condition.good; // Use Prisma.Condition instead
     console.log(`  Adding stuff: ${JSON.stringify(data)}`);
-    // eslint-disable-next-line no-await-in-loop
     await prisma.stuff.upsert({
       where: { id: config.defaultData.indexOf(data) + 1 },
       update: {},
@@ -36,15 +36,20 @@ async function main() {
       },
     });
   }
+
   config.defaultMusicians.forEach(async (musician, index) => {
-    console.log(`  Adding musician: ${musician.name} `);
+    console.log(`  Adding musician: ${musician.name}`);
+    
+    // Ensure genres is an array of strings
+    const genres = Array.isArray(musician.genres) ? musician.genres : [musician.genres];
+
     await prisma.musician.upsert({
       where: { id: index + 1 },
       update: {},
       create: {
         name: musician.name,
         instrument: musician.instrument,
-        genres: musician.genres,
+        genres,  // Ensure genres is an array
         image: musician.image,
         description: musician.description,
         owner: musician.owner,
@@ -52,6 +57,7 @@ async function main() {
     });
   });
 }
+
 main()
   .then(() => prisma.$disconnect())
   .catch(async (e) => {
