578567825e0a4c06a75bdaa381b45b26c4372e93
Yueming Guo <143761227+YuemingGuo9@users.noreply.github.com>
2025-05-05 21:58:14 -1000

    Create supabaseClient.ts

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+// eslint-disable-next-line import/prefer-default-export
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
